c8aa0f4add4431f7b647efb317d533015f93effd
Eric Chang <pkerichang@gmail.com>
2018-04-21 15:02:45 -0700

    1. updated nand gate design script.

--- BagModules/bag_digital_ec/nand.py
+++ BagModules/bag_digital_ec/nand.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from typing import Dict
+from typing import Dict, Any
 
 import os
 import pkg_resources
@@ -24,30 +24,45 @@ class bag_digital_ec__nand(Module):
     @classmethod
     def get_params_info(cls):
         # type: () -> Dict[str, str]
-        """Returns a dictionary from parameter names to descriptions.
-
-        Returns
-        -------
-        param_info : Optional[Dict[str, str]]
-            dictionary from parameter names to descriptions.
-        """
         return dict(
+            nin='number of inputs.',
+            lch='channel length.',
+            wp='PMOS width.',
+            wn='NMOS width.',
+            thp='PMOS threshold.',
+            thn='NMOS threshold.',
+            segp='PMOS segments.',
+            segn='NMOS segments.',
         )
 
-    def design(self):
-        """To be overridden by subclasses to design this module.
+    @classmethod
+    def get_default_param_values(cls):
+        # type: () -> Dict[str, Any]
+        return dict(
+            nin=2,
+        )
 
-        This method should fill in values for all parameters in
-        self.parameters.  To design instances of this module, you can
-        call their design() method or any other ways you coded.
+    def design(self, nin, lch, wp, wn, thp, thn, segp, segn):
+        if segp < 1 or segn < 1:
+            raise ValueError('number of segments must be >= 1.')
+        if nin < 2:
+            raise ValueError('nin must be >= 2.')
+        if nin > 2:
+            raise ValueError('not supported yet.')
+        else:
+            self.instances['XP0'].design(w=wp, l=lch, nf=segp, intent=thp)
+            self.instances['XP1'].design(w=wp, l=lch, nf=segp, intent=thp)
+            if segn == 1:
+                self.instances['XN0'].design(w=wn, l=lch, nf=1, intent=thn)
+                self.instances['XN1'].design(w=wn, l=lch, nf=1, intent=thn)
+            else:
+                self.delete_instance('XN0')
 
-        To modify schematic structure, call:
+                # use array notation to denote parallel segments
+                seg_str = '<%d:0>' % (segn - 1)
+                name_list = ['XN0' + seg_str, 'XN1' + seg_str]
+                term_list = [dict(S='VSS', G='in<0>', D='mid' + seg_str),
+                             dict(S='mid' + seg_str, G='in<1>', D='out')]
 
-        rename_pin()
-        delete_instance()
-        replace_instance_master()
-        reconnect_instance_terminal()
-        restore_instance()
-        array_instance()
-        """
-        pass
+                self.instances['XN1'].design(w=wn, l=lch, nf=1, intent=thn)
+                self.array_instance('XN1', name_list, term_list=term_list)
